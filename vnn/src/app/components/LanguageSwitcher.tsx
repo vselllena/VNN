0cb133b5206031d6721a8fd4206b3cbc3cc3a262
Elena Nikolova <108829233+vselllena@users.noreply.github.com>
2024-10-05 21:54:55 +0300

    language button imrpovements

--- vnn/src/app/components/LanguageSwitcher.tsx
+++ vnn/src/app/components/LanguageSwitcher.tsx
@@ -2,6 +2,7 @@
 
 import { useLanguage } from "../contexts/LanguageContext";
 import { useEffect } from "react";
+import styles from "./LanguageSwitcher.module.css";
 
 export default function LanguageSwitcher() {
   const { language, setLanguage } = useLanguage();
@@ -20,6 +21,19 @@ export default function LanguageSwitcher() {
   };
 
   return (
-    <button onClick={toggleLanguage}>{language === "en" ? "БГ" : "EN"}</button>
+    <div className={styles.toggleContainer}>
+      <input
+        type="checkbox"
+        id="language-toggle"
+        className={styles.toggleInput}
+        checked={language === "bg"}
+        onChange={toggleLanguage}
+      />
+      <label htmlFor="language-toggle" className={styles.toggleLabel}>
+        <span className={styles.toggleButton}></span>
+        <span className={`${styles.toggleText} ${styles.enText}`}>EN</span>
+        <span className={`${styles.toggleText} ${styles.bgText}`}>БГ</span>
+      </label>
+    </div>
   );
 }
