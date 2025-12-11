71b66c725b3d7d8b5bb1b210403003ae5dc2b8e2
Elena Nikolova <nikolovaelena759@gmail.com>
2025-12-11 10:22:10 +0200

    adds footer

--- vnn/src/app/contact/page.tsx
+++ vnn/src/app/contact/page.tsx
@@ -3,6 +3,7 @@
 import { useLanguage } from "../contexts/LanguageContext";
 import Form from "../components/Form";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Contact() {
   const { language } = useLanguage();
@@ -23,14 +24,10 @@ export default function Contact() {
   return (
     <div>
       <Header />
-      <div className="container mx-auto px-4 pt-8">
-        <h1 className="text-3xl font-bold mb-4">{t.title}</h1>
-        <p className="mb-8">{t.description}</p>
+    
         <Form />
-        <p className="my-4 text-xs leading-5 text-center text-gray-500">
-          &copy; 2024 VNN DESIGN, Inc. All rights reserved.
-        </p>
-      </div>
+        <Footer />
+ 
     </div>
   );
 }
